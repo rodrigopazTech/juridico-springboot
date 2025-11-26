e19ced3bdaf3df98f8ef420bbee61994a6aa5ea6
Alam Ramsés Ayala Jimenez <servicio.socialgdsa1@finabien.gob.mx>
2025-11-26 11:55:58 -0600

    Tailwind local

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Aquí le decimos: "Busca en la carpeta de templates de Spring"
+  content: ["./src/main/resources/templates/**/*.html"], 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
