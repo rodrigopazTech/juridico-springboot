88fd65cb8a4d7f5066d330db82efa0d5a171d622
Alam Ramsés Ayala Jimenez <servicio.socialgdsa1@finabien.gob.mx>
2025-12-08 14:17:19 -0600

    pantalla de dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // Aquí le decimos: "Busca en la carpeta de templates de Spring"
   content: ["./src/main/resources/templates/**/*.html"], 
   theme: {
-    extend: {},
+    extend: {
+      // AQUÍ AGREGAMOS LOS COLORES PERSONALIZADOS
+      colors: {
+        gob: {
+          guinda: '#9D2449',
+          guindaDark: '#611232',
+          oro: '#B38E5D',
+          oroLight: '#DDC9A3',
+          gris: '#545454',
+          plata: '#98989A',
+          verde: '#13322B',
+          verdeDark: '#0C231E'
+        }
+      },
+      // Opcional: Si usas fuentes específicas en el HTML
+      fontFamily: {
+        headings: ['Montserrat', 'sans-serif'],
+        sans: ['Noto Sans', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
